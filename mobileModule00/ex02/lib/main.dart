53bdb169fb787771fe4a923f5ed9c0d9eaae45cd
OUAFABULOUS <ouafae1310@gmail.com>
2024-03-20 16:12:21 +0100

    fix responsiveness day00ex02

--- mobileModule00/ex02/lib/main.dart
+++ mobileModule00/ex02/lib/main.dart
@@ -13,8 +13,7 @@ void main() {
               Expanded(
                 child: CalcScreen(),
               ),
-              SizedBox(height: 8.0), // Add this SizedBox for spacing
-              CalcKeyboard(), // Remove the Expanded widget around CalcKeyboard
+              CalcKeyboard(),
             ],
           ))));
 }
@@ -77,55 +76,76 @@ class CalcKeyboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double buttonPadding = screenWidth > 600 ? 12.0 : 8.0;
+    final double ratio = screenWidth > 600 ? 7 : 1.0;
+
+    // No need to pass buttonPadding to createButton method anymore.
     return GridView.count(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      crossAxisCount: 5, // Number of columns in the grid
-      childAspectRatio: 1.0, // Aspect ratio of each button
-      mainAxisSpacing: 8.0, // Space between rows
-      crossAxisSpacing: 8.0, // Space between columns
-      padding: const EdgeInsets.all(8.0), // Add padding around the grid
-      children: [
-        ...createButton('7'),
-        ...createButton('8'),
-        ...createButton('9'),
-        ...createButton('C'),
-        ...createButton('AC'),
-        ...createButton('4'),
-        ...createButton('5'),
-        ...createButton('6'),
-        ...createButton('/'),
-        ...createButton('*'),
-        ...createButton('1'),
-        ...createButton('2'),
-        ...createButton('3'),
-        ...createButton('-'),
-        ...createButton('+'),
-        ...createButton('0'),
-        ...createButton('.'),
-        ...createButton('00'),
-        ...createButton('='),
-        ...createButton('')
-      ],
+      crossAxisCount: 5, // Keep the number of columns to 5
+      childAspectRatio: ratio, // Maintain the button aspect ratio
+      mainAxisSpacing: buttonPadding, // Dynamically adjust the spacing
+      crossAxisSpacing: buttonPadding, // Dynamically adjust the spacing
+      padding: EdgeInsets.all(buttonPadding), // Adjust the grid padding
+      children:
+          List.generate(20, (index) => createButton(getButtonLabel(index))),
     );
   }
 
-  List<Widget> createButton(String buttonText) {
-    return [
-      GridTile(
-        child: ElevatedButton(
-          onPressed: () {
-            print('Button pressed: $buttonText');
-          },
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size(
-                double.infinity,
-                double
-                    .infinity), // This will expand the button to fill the available space
+  Widget createButton(String buttonText) {
+    // Consistent padding for all buttons.
+    return Flexible(
+      child: ElevatedButton(
+        onPressed: () {
+          print('Button pressed: $buttonText');
+        },
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
           ),
-          child: Text(buttonText),
+          // Let the button expand to fill the space instead of setting a minimum size
+        ),
+        child: FittedBox(
+          // To adjust text size automatically
+          fit: BoxFit.scaleDown,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+                vertical: 1,
+                horizontal: 1), // Adjust inner padding if needed
+            child: Text(buttonText),
+          ),
         ),
       ),
+    );
+  }
+
+  String getButtonLabel(int index) {
+    const labels = [
+      '7',
+      '8',
+      '9',
+      'C',
+      'AC',
+      '4',
+      '5',
+      '6',
+      '/',
+      '*',
+      '1',
+      '2',
+      '3',
+      '-',
+      '+',
+      '0',
+      '.',
+      '00',
+      '=',
+      '',
     ];
+    // Your logic to return the button label based on index
+    // Just as an example, returning index as string
+    return labels[index];
   }
 }
